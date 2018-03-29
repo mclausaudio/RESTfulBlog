3a75209bca57708ed3df38da794782f2a8a477ab
runsf <mclausaudio@gmail.com>
2018-03-29 03:54:38 +0000

    added show route and template

--- app.js
+++ app.js
@@ -55,6 +55,16 @@ app.post("/blogs", function(req, res){
     })
     //redirect
 })
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    })
+})
 
 
 
